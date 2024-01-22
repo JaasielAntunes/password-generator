546462b3cc8a195d41e572437edcc6c7b5f2c8d1
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-22 11:46:52 -0300

    Métodos para gerar números aleatórios, símbolos, letras maiúsculas e minúsculas

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ const numbersInput = document.querySelector(".numbers");
 const symbolsInput = document.querySelector(".symbols");
 const copyPasswordButton = document.querySelector(".copy-password");
 
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,#!@.$%&*+-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
