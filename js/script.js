6178ae148287cbd995726c8fb30940e2a11eee05
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-22 11:42:19 -0300

    Seleção de elementos para manipulação

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const generatedPasswordElement = document.querySelector(".generated-password");
+const generatePasswordButton = document.querySelector(".generate-password");
+
+const lengthInput = document.querySelector(".length");
+const lettersInput = document.querySelector(".letters");
+const numbersInput = document.querySelector(".numbers");
+const symbolsInput = document.querySelector(".symbols");
+const copyPasswordButton = document.querySelector(".copy-password");
+
